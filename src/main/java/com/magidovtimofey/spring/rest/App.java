c9fe0bebec5179ee2c9eebace9f5d0d31a3fbced
1user1-1 <120933718+1user1-1@users.noreply.github.com>
2023-09-24 12:47:08 +0300

    Communication between App and Client has been made.

--- src/main/java/com/magidovtimofey/spring/rest/App.java
+++ src/main/java/com/magidovtimofey/spring/rest/App.java
@@ -1,13 +1,34 @@
 package com.magidovtimofey.spring.rest;
 
+import com.magidovtimofey.spring.rest.configuration.MyConfig;
+import com.magidovtimofey.spring.rest.entity.Employee;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(MyConfig.class);
+
+        Communication communication = context.getBean("communication",
+                Communication.class);
+
+//        List<Employee> allEmployees = communication.getAllEmployees();
+//        System.out.println(allEmployees);
+
+//        Employee empById = communication.getEmployee(1);
+//        System.out.println(empById);
+
+//        Employee emp = new Employee("Sveta", "Sokolova"
+//                , "IT", 1200);
+//        emp.setId(11);
+//        communication.saveEmployee(emp);
+        communication.deleteEmployee(11);
     }
 }
